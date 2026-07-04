4d23f7ce7ee2366d6fc557765a559170a9edfa2e
Amster <traincrusher1@gmail.com>
2026-07-04 14:33:07 -0400

    Core Simulation rendering pipeline

---
+++ src/Core/Simulation.cs
@@ -0,0 +1,44 @@
+﻿namespace Core;
+using Raylib_cs;
+
+public abstract class Simulation
+{
+
+    // Core class for rendering.
+
+    // Function to initialize window of width, height with title
+    public void Run(int width, int height, string title)
+    {
+        // Basic initialization
+        Raylib.InitWindow(width, height, title);
+        Raylib.SetTargetFPS(60);
+
+        Initialize();
+
+        // Draw loop
+        while (!Raylib.WindowShouldClose())
+        {
+            Update(Raylib.GetFrameTime()); // deltatime
+            Raylib.BeginDrawing();
+            Draw();
+            Raylib.DrawFPS(10, 10);
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+
+    // Basic initialization
+    protected virtual void Initialize()
+    {
+        Console.WriteLine("Initializing...");
+    }
+
+    // Runs every frame, framerate independent via deltaTime
+    protected virtual void Update(float deltaTime)
+    {
+    }
+
+    // Overrideable class to render with
+    protected abstract void Draw();
+}
---
+++ src/SuiteApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Core;
+using Raylib_cs;
+
+class Program : Core.Simulation
+{
+
+    float xPos = 100f;
+
+    static void Main()
+    {
+        Program myApp = new Program();
+        myApp.Run(800, 500, "Test");
+    }
+
+    protected override void Initialize()
+    {
+
+    }
+
+    protected override void Update(float deltaTime)
+    {
+        xPos += deltaTime * 50;
+    }
+
+    protected override void Draw()
+    {
+        Raylib.ClearBackground(new Color(20, 20, 20, 255));
+        Raylib.DrawCircle((int) xPos, 100, 20, new Color(255, 255, 255, 255));
+    }
+}
